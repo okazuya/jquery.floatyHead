aeecb567acf043754d95016d4e826f493561a9c3
okazuya <okazuya@localhost>
2012-09-17 11:53:06 +0900

    FirefoxやChromeでのずれ対策
    FirefoxやChromeで横スクロールしたときにずれることがあった。
    IE8では現象は見られなかった。

--- jquery.floatyHead.js
+++ jquery.floatyHead.js
@@ -37,31 +37,8 @@ Notes:
 */
 
 (function( $ ){
-	var settings = {
-		positionFixed: false
-	}
-	
 	var methods = {
 		init : function( options ) {
-			var container = document.body;
-			if (document.createElement && container && container.appendChild && container.removeChild) {
-				var el = document.createElement("div");
-				if (!el.getBoundingClientRect) {
-					return null;
-				}
-				el.innerHTML = "x";
-				el.style.cssText = "position:fixed;top:100px;";
-				container.appendChild(el);
-				var originalHeight = container.style.height, originalScrollTop = container.scrollTop;
-				container.style.height = "3000px";
-				container.scrollTop = 500;
-				var elementTop = el.getBoundingClientRect().top;
-				container.style.height = originalHeight;
-				settings.positionFixed = elementTop === 100;
-				container.removeChild(el);
-				container.scrollTop = originalScrollTop;
-			}
-			
 			$(window).bind('scroll.floatyHead', methods.update);
 			$(window).bind('resize.floatyHead', methods.update_sizes);
 			
@@ -82,14 +59,7 @@ Notes:
 				var MaxOffset = $(this).data('maxOffset');
 				
 				if( ScrollTop > Offset.top && ScrollTop < MaxOffset ) {
-					if( settings.positionFixed ) {
-						$(this).css('top', 0).show();
-						if( ScrollLeft != 0 ) {
-							$(this).css('left', -ScrollLeft + 'px');
-						}
-					} else {
-						$(this).css('position','absolute').css('top', ScrollTop + 'px').css('left', Offset.left + 'px').show();
-					}
+					$(this).css('position','absolute').css('top', ScrollTop + 'px').css('left', Offset.left + 'px').show();
 				} else {
 					$(this).hide();
 				}
